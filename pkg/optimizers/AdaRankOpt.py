2fe50adc30e0dcfae4b50889a136f2ec52739a60
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-11-06 12:28:33 +0100

    polyhedral set

--- pkg/optimizers/AdaRankOpt.py
+++ pkg/optimizers/AdaRankOpt.py
@@ -3,55 +3,9 @@
 #
 
 import numpy as np
+import scipy.special as sp
+from scipy.optimize import linprog
 from .optimizer import Optimizer
-
-
-def polynomial_map(x, k):
-    """
-    Mapping from R^d to the polynomial feature space of degree k.
-
-    Parameters
-    ----------
-    x : np.ndarray
-        The input vector.
-    k : int
-        The degree of the polynomial.
-
-    Returns
-    -------
-    set
-        The mapped vector.
-    """
-    sym = [f"{i}" for i in range(x.shape[0])]
-
-    def up_to_permutation(e, l):
-        for i in range(len(l)):
-            if e == l[i]:
-                return True
-            # Check if e is a permutation of l[i]
-            if len(e) == len(l[i]) and sorted(e) == sorted(l[i]):
-                return True
-        return False
-
-    def aux(k):
-        if k == 1:
-            return sym
-        else:
-            x_k_m_1 = aux(k - 1)
-            x_k_1 = x_k_m_1.copy()
-            for a in sym:
-                for b in x_k_m_1:
-                    if not up_to_permutation(f"{b} {a}", x_k_1):
-                        x_k_1.append(f"{a} {b}")
-            return x_k_1
-
-    operations = aux(k)
-    res = np.ones(len(operations))
-    for i, op in enumerate(operations):
-        splt = op.split(" ")
-        for s in splt:
-            res[i] *= x[int(s)]
-    return res
 
 
 class AdaRankOpt(Optimizer):
@@ -59,3 +13,110 @@ class AdaRankOpt(Optimizer):
         super().__init__("AdaRankOpt", bounds)
         self.n_eval = n_eval
         self.p = p
+
+    @staticmethod
+    def polynomial_map(x, k):
+        """
+        Mapping from R^d to the polynomial feature space of degree k.
+
+        Parameters
+        ----------
+        x : np.ndarray
+            The input vector.
+        k : int
+            The degree of the polynomial.
+
+        Returns
+        -------
+        np.ndarray
+            The mapped vector.
+        """
+        sym = [f"{i}" for i in range(x.shape[0])]
+
+        def aux(k):
+            if k == 1:
+                return sym
+            else:
+                phi_k_m_1 = aux(k - 1)
+                x_k = phi_k_m_1.copy()
+                phi_k_m_1_only = []
+                for e in phi_k_m_1:
+                    if len(e.split(" ")) == k - 1:
+                        phi_k_m_1_only.append(e)
+                for a in sym:
+                    phi_k_m_1_only_c = phi_k_m_1_only.copy()
+                    for b in phi_k_m_1_only:
+                        x_k.append(f"{a} {b}")
+                        if a in b:
+                            phi_k_m_1_only_c.remove(b)
+                    phi_k_m_1_only = phi_k_m_1_only_c
+                return x_k
+
+        operations = aux(k)
+        assert len(operations) == sp.comb(x.shape[0] + k, x.shape[0]) - 1
+        res = np.ones(len(operations))
+        for i, op in enumerate(operations):
+            splt = op.split(" ")
+            for s in splt:
+                res[i] *= x[int(s)]
+        return res
+
+    @staticmethod
+    def polynomial_matrix(X, k):
+        """
+        Constructs the polynomial matrix of degree k from the input matrix X, using the polynomial_map function.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input matrix.
+        k : int
+            The degree of the polynomial.
+
+        Returns
+        -------
+        np.ndarray
+            The polynomial matrix.
+        """
+        d = X.shape[1]
+        n = X.shape[0] - 1
+        M = np.zeros((n, int(sp.comb(d + k, d) - 1)))
+        for i in range(n):
+            M[i] = AdaRankOpt.polynomial_map(X[i + 1], k) - AdaRankOpt.polynomial_map(
+                X[i], k
+            )
+        return M
+
+    @staticmethod
+    def is_polyhedral_set_empty(X, k):
+        """
+        Checks if the polyhedral set constructed with the polynomial_matrix function is empty using linear programming.
+
+        Parameters
+        ----------
+        X : np.ndarray
+            The input matrix.
+        k : int
+            The degree of the polynomial.
+
+        Returns
+        -------
+        np.ndarray
+            The polyhedral set.
+        """
+        n = X.shape[0] - 1
+        M = AdaRankOpt.polynomial_matrix(X, k).T
+        M = np.vstack((M, np.ones(n)))
+        b_eq = np.zeros(M.shape[0])
+        b_eq[-1] = 1
+        print("M", M)
+        print("M.shape", M.shape)
+        print("b_eq", b_eq)
+        res = linprog(np.ones(n), A_eq=M, b_eq=b_eq, options={"maxiter": 10})
+        print(res)
+        return res.status == 2
+
+    def test(self):
+        d = 5
+        X = np.random.uniform(-3, 3, (3, d))
+        print(self.is_polyhedral_set_empty(X, 10))
