e70d128a6a550d75bd53f2265f37f467095221c7
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-11-22 16:30:34 +0100

    Version

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from setuptools import setup
 from extensions import *
+from toml import load
+
+version = load("pyproject.toml")["project"]["version"]
 
 setup(
-    version="0.1.0",
+    version=version,
     setup_requires=["numpy==2.1.3"],
-    ext_modules=[OptBuildExtension("gob", "0.1.0")],
+    ext_modules=[OptBuildExtension("gob", version)],
     cmdclass={"build_ext": OptBuild},
     zip_safe=False,
 )
