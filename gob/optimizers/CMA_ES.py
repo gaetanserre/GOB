deb464660fe648849f1def5f0bc2ebbd3bc26866
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2024-11-28 12:39:40 +0100

    CMA-ES parameters order

--- gob/optimizers/CMA_ES.py
+++ gob/optimizers/CMA_ES.py
@@ -7,7 +7,7 @@ from .cpp_optimizers import CMA_ES as C_CMA_ES
 
 
 class CMA_ES(Optimizer):
-    def __init__(self, bounds, m_0=None, n_eval=1000, sigma0=1):
+    def __init__(self, bounds, n_eval=1000, m_0=None, sigma0=1):
         super().__init__("CMA-ES", bounds)
         self.c_opt = C_CMA_ES(bounds, n_eval, [] if m_0 is None else m_0, sigma0)
 
