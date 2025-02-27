876d89088b8bfc2db53a7ee66e5fa0a7f3847062
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-02-27 11:49:00 +0100

    Windows compile

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from setuptools import setup
 from extensions import *
 from toml import load
 
+name = load("pyproject.toml")["project"]["name"]
 version = load("pyproject.toml")["project"]["version"]
 
 setup(
+    name=name,
     version=version,
     setup_requires=["numpy==2.1.3"],
     ext_modules=[OptBuildExtension("gob", version)],
