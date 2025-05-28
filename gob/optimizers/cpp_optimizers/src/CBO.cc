17690413d9560e3a0e687ede59e0e975b3140356
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-05-28 12:07:55 +0200

    CBO smooth heaviside

--- gob/optimizers/cpp_optimizers/src/CBO.cc
+++ gob/optimizers/cpp_optimizers/src/CBO.cc
@@ -16,6 +16,11 @@ dyn_vector compute_vf(Eigen::MatrixXd &particles, dyn_vector weights)
   return v / sum_weights;
 }
 
+double smooth_heaviside(double x)
+{
+  return (1 / 2) * erf(x) + 1 / 2;
+}
+
 dyn_vector CBO::weights(Eigen::MatrixXd &particles, function<double(dyn_vector x)> f, vector<double> *evals)
 {
   dyn_vector w(particles.rows());
@@ -23,7 +28,7 @@ dyn_vector CBO::weights(Eigen::MatrixXd &particles, function<double(dyn_vector x
   {
     double f_x = f(particles.row(i));
     (*evals)[i] = f_x;
-    w[i] = exp(-this->lambda * f_x);
+    w[i] = exp(-this->alpha * f_x);
   }
   return w;
 }
@@ -37,5 +42,12 @@ Eigen::MatrixXd CBO::dynamics(function<double(dyn_vector x)> f, int &time, Eigen
     vf_matrix_dupl.row(i) = vf;
   }
   Eigen::MatrixXd particles_diff = particles - vf_matrix_dupl;
-  return -this->lambda * particles_diff + sqrt(2) * this->sigma * particles_diff * unif_random_normal(this->re, 0, this->lambda);
+  Eigen::MatrixXd heaviside(particles_diff.rows(), particles_diff.cols());
+  double f_vf = f(vf);
+  for (int i = 0; i < particles_diff.rows(); i++)
+  {
+    heaviside.row(i) = particles_diff.row(i) * smooth_heaviside((1.0 / this->epsilon) * ((*evals)[i] - f_vf));
+  }
+
+  return -this->lambda * heaviside + sqrt(2) * this->sigma * particles_diff * unif_random_normal(this->re, 0, this->lambda);
 }
--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -9,10 +9,14 @@ pygkls = PyGKLS(2, 15, [-5, 5], -100, smoothness="ND", deterministic=True)
 
 f = Square()
 
-opt = CBO(bounds=create_bounds(2, -5, 5))
+opt = CBO(bounds=create_bounds(2, -5, 5), n_particles=500, iter=1000)
 res = opt.minimize(f)
 print(f"Results for {opt} : {res[1]}")
 
 opt = SBS(bounds=create_bounds(2, -5, 5))
+res = opt.minimize(f)
+print(f"Results for {opt} : {res[1]}")
+
+opt = AdaLIPO_P(bounds=create_bounds(2, -5, 5))
 res = opt.minimize(f)
 print(f"Results for {opt} : {res[1]}")
