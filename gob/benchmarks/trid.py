0780ad4ec0e3817ef827a6a1bb652531be51bca8
Gwendal200 <gwendal.deb@gmail.com>
2025-09-30 14:19:32 +0200

    update docstring

--- gob/benchmarks/trid.py
+++ gob/benchmarks/trid.py
@@ -10,6 +10,8 @@ class Trid(Benchmark):
     """
     The Trid function.
     The function is normalized according to the dimension to ensure consistency in the global minima.
+    :math:`x : \\to \\sum_{i=1}^d (x_i - 1)^2 - \\sum_{i=2}^d x_i x_{i-1}`
+    minimum is achieved at :math:`x_i = i(d + 1 - i) \\forall i`
     """
 
     def __init__(self):
