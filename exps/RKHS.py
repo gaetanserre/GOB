8c8b679b27d8b5e14eb97444723beaa9e6c52a6e
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-12-13 17:17:57 +0100

    RKHS exp

---
+++ exps/RKHS.py
@@ -0,0 +1,66 @@
+#
+# Created in 2025 by Gaëtan Serré
+#
+
+from utils import (
+    print_avg_rank,
+    noisy_functions,
+    noisy_functions_bounds,
+    flat_functions,
+    flat_functions_bounds,
+    smooth_functions,
+    smooth_functions_bounds,
+)
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from gob import GOB
+
+n_particles = 75
+iter = 300
+sigma = 1 / n_particles**2
+
+if __name__ == "__main__":
+    algorithms = [
+        ("SBS", {"n_particles": n_particles, "iter": iter, "sigma": sigma}),
+        (
+            "SBS-RKHS",
+            {"n_particles": n_particles, "iter": iter, "sigma": sigma},
+        ),
+    ]
+
+    # noisy
+    gob = GOB(
+        algorithms,
+        noisy_functions,
+        [],
+        bounds=noisy_functions_bounds,
+    )
+    print("Running noisy functions experiments...")
+    res_dict = gob.run(n_runs=20, verbose=1, latex_table=True)
+    print_avg_rank(res_dict)
+
+    # flat
+    gob = GOB(
+        algorithms,
+        flat_functions,
+        [],
+        bounds=flat_functions_bounds,
+    )
+    print("Running flat functions experiments...")
+    res_dict = gob.run(n_runs=20, verbose=1, latex_table=True)
+    print_avg_rank(res_dict)
+
+    # smooth
+    gob = GOB(
+        algorithms,
+        smooth_functions,
+        [],
+        bounds=smooth_functions_bounds,
+    )
+    print("Running smooth functions experiments...")
+    res_dict = gob.run(n_runs=20, verbose=1, latex_table=True)
+    print_avg_rank(res_dict)
